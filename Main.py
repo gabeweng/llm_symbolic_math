688bbc913ebf9d7085bff536daf2c17860dbd023
gabeweng <gabeweng@gmail.com>
2024-01-28 14:52:27 -0500

    define llm

--- Main.py
+++ Main.py
@@ -2,6 +2,10 @@ from langchain_experimental.llm_symbolic_math.base import LLMSymbolicMathChain
 from langchain_openai import OpenAI
 import streamlit as st
 
+llm = OpenAI(temperature=0)
+llm_symbolic_math = LLMSymbolicMathChain.from_llm(llm)
+
 st.title("Symbolic Math Chain")
 
 question=st.text_input("Enter your question here")
+
