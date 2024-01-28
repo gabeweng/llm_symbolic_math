f1a210d9d88857823ff0ef4542aaa360e927d360
gabeweng <gabeweng@gmail.com>
2024-01-28 14:52:08 -0500

    streamlit inputs

--- Main.py
+++ Main.py
@@ -1,3 +1,7 @@
 from langchain_experimental.llm_symbolic_math.base import LLMSymbolicMathChain
 from langchain_openai import OpenAI
+import streamlit as st
 
+st.title("Symbolic Math Chain")
+
+question=st.text_input("Enter your question here")
